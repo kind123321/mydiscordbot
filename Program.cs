7aab02634e15638026cba2a8684abc7580909931
kind123321 <129680339+kind123321@users.noreply.github.com>
2023-04-04 17:06:12 +0300

    игра с угаданием числа
    проблема в том, что когда идет вопрос да или нет, при вводе нет все норм, но и при вводе да игра закнчивается, как пофиксить?

--- Program.cs
+++ Program.cs
@@ -1,142 +1,73 @@
-﻿using System.Reflection;
-using System.Threading.Tasks;
-using Discord;
-using Discord.Net;
-using Discord.Audio;
-using Discord.Commands;
-using Discord.WebSocket;
-using YoutubeExplode;
-using YoutubeExplode.Videos.Streams;
-using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using System.Web;
+using System;
 
-public class MusicModule : ModuleBase<SocketCommandContext>
+namespace HelloWorld
 {
-
-    private readonly YoutubeClient _ytClient;
-
-    public MusicModule(YoutubeClient ytClient)
+    class Program
     {
-        _ytClient = ytClient;
-    }
-
-    [Command("play")]
-    public async Task PlayMusicAsync([Remainder] string songName)
-    {
-        string videoId = null;
-        if (Uri.TryCreate(songName, UriKind.Absolute, out Uri uri) && uri.Host == "www.youtube.com")
+        public static int GenerateRandomNumber()
         {
-            var queryParams = HttpUtility.ParseQueryString(uri.Query);
-            videoId = queryParams["v"];
-        }
-        else if (Uri.TryCreate(songName, UriKind.Absolute, out uri) && uri.Host == "youtu.be")
-        {
-            videoId = uri.AbsolutePath.TrimStart('/');
-        }
-        else
-        {
-            videoId = (await _ytClient.Search.GetVideosAsync(songName).FirstOrDefaultAsync())?.Id;
+            Random random = new Random();
+            return random.Next(1, 6);
         }
 
-        if (videoId == null)
+        public static bool PlayAgain()
         {
-            await Context.Channel.SendMessageAsync("Video not found!");
-            return;
-        }
+            Console.WriteLine("\nХотите сыграть еще раз? Напишите \"да\", чтобы сыграть еще раз, и \"нет\", чтобы закрыть программу.");
 
-        var streamManifest = await _ytClient.Videos.Streams.GetManifestAsync(videoId);
-        var streamInfo = streamManifest.GetAudioStreams().GetWithHighestBitrate();
-        var stream = await _ytClient.Videos.Streams.GetAsync(streamInfo);
+            string playAgainInput = Console.ReadLine();
 
-        var voiceChannel = (Context.User as IGuildUser)?.VoiceChannel;
-        if (voiceChannel == null)
-        {
-            await Context.Channel.SendMessageAsync("You must be in a voice channel!");
-            return;
-        }
-
-        var audioClient = await voiceChannel.ConnectAsync();
-        var audioStream = audioClient.CreatePCMStream(AudioApplication.Music);
-        await stream.CopyToAsync(audioStream);
-        await audioStream.FlushAsync();
-        await audioClient.StopAsync();
-    }
-}
-public class MusicBot
-{
-   
-
-    private readonly DiscordSocketClient _client;
-    private readonly CommandService _commands;
-    private readonly YoutubeClient _ytClient;
-    static async Task Main(string[] args)
-    {
-      
-
-        var bot = new MusicBot();
-        await bot.MainAsync("MTA3NjgzNTMzNjg5NTg3MzAyNA.GkABFJ.KYsnRvMZ3kVW4NxdiJyGKU_9UjDv6fOxmDh9fc");
-
-
-
-    }
-
-    public MusicBot()
-    {
-        _client = new DiscordSocketClient();
-        _commands = new CommandService();
-        _ytClient = new YoutubeClient();
-
-        var config = new DiscordSocketConfig { GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages };
-        _client = new DiscordSocketClient(config);
-       
-
-
-    }
-
-    public async Task MainAsync(string token)
-    {
-
-        _client.Log += Log;
-        _commands.Log += Log;
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        var services = new ServiceCollection()
-            .AddSingleton(_ytClient)
-            .BuildServiceProvider();
-
-        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
-
-        _client.MessageReceived += HandleCommandAsync;
-
-        await Task.Delay(-1);
-    }
-
-    private async Task HandleCommandAsync(SocketMessage messageParam)
-    {
-        Console.WriteLine();
-        var message = messageParam as SocketUserMessage;
-        if (message == null) return;
-
-        var context = new SocketCommandContext(_client, message);
-
-        int argPos = 0;
-        if (message.HasStringPrefix("&", ref argPos))
-        {
-            var result = await _commands.ExecuteAsync(context, argPos, null);
-
-            if (!result.IsSuccess)
+            if (playAgainInput.ToLower() == "да")
             {
-                Console.WriteLine(result.ErrorReason); 
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nСпасибо за игру! До свидания!");
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                int number = 0, myNum, attempts = 2;
+                Console.WriteLine("Я тинни, я люблю загадывать числа. Попробуйте угадать! (Я загадал число от 1 до 5)\nВведите свое число:");
+
+                myNum = GenerateRandomNumber();
+
+                while (attempts > 0)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out number))
+                    {
+                        Console.WriteLine("Неверный ввод! Введите число от 1 до 5:");
+                        continue;
+                    }
+
+                    if (myNum == number)
+                    {
+                        Console.WriteLine("Вы угадали! Мое число: " + myNum);
+                        Console.WriteLine("\nДавайте реванш!\n");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы не угадали! Осталось попыток: " + --attempts);
+                    }
+                }
+
+                if (attempts == 0)
+                {
+                    Console.WriteLine("Вы проиграли! Мое число: " + myNum);
+                }
+
+                playAgain = PlayAgain();
             }
         }
     }
 
-    private Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
 }
